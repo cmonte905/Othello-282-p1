6a741c886d1fa09f12ac7b7056c732bc4a452c75
cmonte905 <cmonte905@hotmail.com>
2015-02-28 01:10:10 -0800

    Apply move not done but comments are removed

--- OthelloGame.cpp
+++ OthelloGame.cpp
@@ -33,7 +33,8 @@ bool IsValidMove(char board[BOARD_SIZE][BOARD_SIZE], int row, int col) {
 
 
 void GetMove(int *row, int *col) {
-
+   char temp;
+   cin >> temp >> *row >> temp >> *col >> temp;
 }
 
 //switch with elvis cases
@@ -44,7 +45,6 @@ void ApplyMove(char board[BOARD_SIZE][BOARD_SIZE], int row
          int r = row + i, c = col + j;
          int count = 0, player = (currentPlayer == 'B' ? 1 : -1);
          while (board[r][c] != 0 && InBounds(r, c)) {
-
             if (board[r][c] == (currentPlayer == 'B' ? -1 : 1)) {
                ();
                ();
@@ -69,53 +69,3 @@ int GetValue(char board[BOARD_SIZE][BOARD_SIZE]) {
       }
    }
 }
-/*
-My codex
-char currentPlayer) {
-int count = 0, player = (currentPlayer == 'B' ? 1 : -1);
-for (int i = -1; i <= 1; i++) {
-for (int j = -1; j <= 1; j++) {
-int r = row + i, c = col + j;
-while (board[r][c] != 0 && InBounds(r, c)) {
-if (board[row - i][col - j] == i) {
-
-}
-else {
-false;
-}
-}
-}
-}
-
-Richs code for testing
-void ApplyMove(char board[BOARD_SIZE][BOARD_SIZE], int row
-, int col, char currentPlayer) {
-stack<char*> dir;
-int player = ((currentPlayer == 'B') ? 1 : -1);
-for (int r = -1; r <= 1; r++) { //check every direction
-for (int c = -1; c <= 1; c++) {
-int bRow = row + r, bCol = col + c; //go in that direction
-while (InBounds(bRow, bCol) && (board[bRow][bCol] != 0)) { //while valid (in bound and not empty)
-cout << "I dont think it fucking works"<<endl;
-if (board[bRow][bCol] != player) //true if that room contain enemy
-dir.push(&board[bRow][bCol]);
-else { //room contain player
-if (!dir.empty()) {
-while (!dir.empty()) {//traverse back
-//(dir.pop()) = currentPlayer;
-*(dir.top()) = currentPlayer;
-dir.pop();
-}
-break;
-}
-bRow += r, bCol += c;
-}
-}
-}
-}
-}
-
-
-
-*/
-
