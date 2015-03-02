dca83daa1a59bbce46af8a53eb68fd2576b32bc2
cmonte905 <cmonte905@hotmail.com>
2015-03-02 13:16:52 -0800

    Finsihed project. Might be some style issues that will get addressed later on

--- Main Game.cpp
+++ Main Game.cpp
@@ -4,42 +4,55 @@ using namespace std;
 
 int main() {
    bool gameState = true;
-   char board[BOARD_SIZE][BOARD_SIZE];
-   int row, col, turn = 0;
+   char board[BOARD_SIZE][BOARD_SIZE], move;
+   int row, col, turn = 0, skip = 0;
    for (int row = 0; row < BOARD_SIZE; row++) {
       for (int col = 0; col < BOARD_SIZE; col++) {
          board[row][col] = 0;
       }
    }
    cout << "Welcome to Othello" << endl;
-   board[3][4] = 1; 
+   board[3][4] = 1;
    board[4][4] = -1;
-   board[4][3] = 1; 
+   board[4][3] = 1;
    board[3][3] = -1;
    PrintBoard(board);
    while (gameState) {
-      cout << "Black players turn, enter a move" << endl;
-      GetMove(&row, &col);
-      if (InBounds(row, col)) {
-         ApplyMove(board, row, col, 'B');
+      if (turn % 2 == 0) {
+         cout << "Black players turn, enter a move" << endl;
       }
       else {
-         cout << "Move is invalid, please enter a valid move" << endl;
-         GetMove(&row, &col);         
+         cout << "White players turn, enter a move" << endl;
       }
-      PrintBoard(board);
-      cout << "White players turn, enter a move" << endl;
       GetMove(&row, &col);
-      if (InBounds(row, col)) {
-         ApplyMove(board, row, col, 'W');
-         PrintBoard(board);
-      }
-      else {
+      while (!(IsValidMove(board, row, col))) {
          cout << "Move is invalid, please enter a valid move" << endl;
          GetMove(&row, &col);
       }
+      char playerTurn = (turn % 2 == 0 ? 'B' : 'W');
+      ApplyMove(board, row, col, playerTurn);
+      turn++;
+      if (row == -1 && col == -1) {
+         skip++;
+         cout << "Have skipped your turn" << endl;
+         if (skip == 2)
+            break;
+      }
+      else {
+         skip = 0;
+         PrintBoard(board);
+      }
    }
-   //GetValue(board);
+   int tempWin = GetValue(board);
+   if (tempWin > 0) {
+      cout << "Black wins " << endl;
+   }
+   else if (tempWin < 0) {
+      cout << "White wins" << endl;
+   }
+   else {
+      cout << "It is a tie" << endl;
+   }
    system("pause");
    return 0;
 }
--- OthelloGame.cpp
+++ OthelloGame.cpp
@@ -22,148 +22,58 @@ void PrintBoard(char board[BOARD_SIZE][BOARD_SIZE]) {
    }
 }
 bool InBounds(int row, int col) {
-   return (row < BOARD_SIZE) && (row >= 0) && (col < BOARD_SIZE) && (col >= 0);
+   return (row < BOARD_SIZE) && (row > -1) && (col < BOARD_SIZE) && (col > -1);
 }
 
 bool IsValidMove(char board[BOARD_SIZE][BOARD_SIZE], int row, int col) {
-   return (InBounds(row, col) && board[row][col] == 0);
+   return (InBounds(row, col) && (board[row][col] == 0)) ||
+      ((row == -1 && col == -1));
 }
 
 void GetMove(int *row, int *col) {
    char temp;
-   cin >> *row >> temp >> *col; // easier for testing, get rid of when done 
-   //cin >> temp >> *row >> temp >> *col >> temp;
+   cin >> temp >> *row >> temp >> *col >> temp;
 }
 
 
-//switch with elvis cases
 void ApplyMove(char board[BOARD_SIZE][BOARD_SIZE], int row,
    int col, char currentPlayer) {
-
-   bool moreThanOne = true;
    int count = 0, current = (currentPlayer == 'B' ? 1 : -1);
    int opposite = (currentPlayer == 'B' ? -1 : 1);
-   cout << "Currently in applyMove " << current << endl;
    board[row][col] = current;
    for (int i = -1; i < 2; i++) {
       for (int j = -1; j < 2; j++) {
-         int checkR = row + i, checkC = col + j;
-         while (board[checkR][checkC] == opposite) {
-            checkR += i; checkC += j;
+         int cRow = row + i, cCol = col + j;
+         count = 0;
+         while (board[cRow][cCol] == opposite && InBounds(cRow, cCol)) {
+            cRow += i; cCol += j;
             count++;
-            if (board[checkR][checkC] == current && InBounds(checkR, checkC)) {   //&& InBounds(checkR, checkC)
+            if (board[cRow][cCol] == current) {
                break;
             }
          }
-
-
-         if (board[checkR][checkC] == current) {
+         if (board[cRow][cCol] == current) {
             for (;;) {
-               cout << " Piece that should get swithced " << checkR << " " << checkC << endl;
-               for (; count > -1; count--) {
-                  checkR -= i; checkC -= j;
-                  board[checkR][checkC] = current;
+               for (; count >= 0; count--) {
+                  cRow -= i; cCol -= j;
+                  board[cRow][cCol] = current;
                }
-               if (board[checkR][checkC] == board[row][col]) {
+               if (board[cRow][cCol] == board[row][col]) {
                   break;
                }
             }
          }
-
       }
-
-      //PrintBoard(board);
    }
 }
 
-/*
-
-cout << "I fucking hate programming this shit, fuck the 20 line limit " << c << endl;
-c++;
-if (c == 5) {
-break;
-}
-break;
-
-
-
-else {//If there were any pieces to move, then it would
-if (count > 0) {
-for (; count > 0; count--) { // SHould be going back to flip any pieces if there were any
-cout << " Piece that should get swithced" << checkR << " " << checkC << endl;
-checkR -= i; checkC -= j;
-board[checkR][checkC] = current;
-cout << "---------------------------" << endl;
-PrintBoard(board);
-cout << "---------------------------" << endl;
-}
-}
-}
-
-void ApplyMove(char board[BOARD_SIZE][BOARD_SIZE], int row
-, int col, char currentPlayer) {
-//stack<char*> dir;
-int player = ((currentPlayer == 'B') ? 1 : -1);
-for (int r = -1; r <= 1; r++) { //check every direction
-for (int c = -1; c <= 1; c++) {
-int bRow = row + r, bCol = col + c, count = 0;; //go in that direction
-while (InBounds(bRow, bCol) && (board[bRow][bCol] != 0)) { //while valid (in bound and not empty)
-if (board[bRow][bCol] != player) //true if that room contain enemy
-count++;
-else { //room contain player
-if (count > 0) {
-for (; count > 0; count--) {
-bRow--, bCol--;
-board[bRow][bCol] = player;
-}
-break;
-}
-}
-bRow += r, bCol += c;
-}
-
-}
-}
-}
-void ApplyMove(char board[BOARD_SIZE][BOARD_SIZE], int row, int col, char currentPlayer) {
-int changeRow, changeCol, i, j = 0;
-char opposingPlayer = (currentPlayer == 'B') ? 1 : -1;
-board[row][col] = currentPlayer;
-for (changeRow = -1; changeRow < 2; changeRow++)
-for (changeCol = -1; changeCol < 2; changeCol++) {
-
-if (board[row + changeRow][col + changeCol] == opposingPlayer) {
-i = row + changeRow;
-j = col + changeCol;
-for (;;) {
-i += changeRow;
-j += changeCol;
-if (!(InBounds(i, j)))
-break;
-if (board[i][j] == 0)
-break;
-if (board[i][j] == currentPlayer) {
-while (board[i -= changeRow][j -= changeCol] == opposingPlayer)
-board[i][j] = currentPlayer;
-break;
-}
-}
-}
-}
-}
-
-
-
-
-
-
 int GetValue(char board[BOARD_SIZE][BOARD_SIZE]) {
-for (int i = 0; i < BOARD_SIZE; i++) {
-for (int j = 0; j < BOARD_SIZE; j++) {
-int b = 0, w = 0, sum = 0;
-(board[i][j] == 1 ? b++ : w++);
-return (sum = b);
+   int sum = 0;
+   for (int i = 0; i < BOARD_SIZE; i++) {
+      for (int j = 0; j < BOARD_SIZE; j++) {
+         sum += board[i][j];
+      }
+   }
+   return sum;
 }
-}
-}
-*/
+
